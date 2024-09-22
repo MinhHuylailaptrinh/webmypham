2c7a1e535e14c0a17382c850f6ce50fd471e19b9
Minhhuylailaptrinh <biti7777777@gmail.com>
2024-09-22 12:25:56 +0700

    about.php

--- about.php
+++ about.php
@@ -18,7 +18,7 @@
         href="https://fonts.googleapis.com/css?family=El+Messiri:400,500,600,700|Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,700,700i,800,800i,900,900i"
         rel="stylesheet" />
     <link rel="icon" href="favicon.png" type="image/png" />
-    <title>About</title>
+    <title>Hello</title>
 </head>
 
 <body>
